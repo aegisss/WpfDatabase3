083ad863a0dc8b73ed9ecbd23b01c13df66da17e
unknown <[200~isip_a.s.grachev@mpt.ru>
2023-03-18 10:49:53 +0300

    ура победа

---
+++ EmployeeWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Database.MagazineDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Database
+{
+    public partial class EmployeeWindow : Window
+    {
+        EmployeeTableAdapter adapter1 = new EmployeeTableAdapter();
+        public EmployeeWindow()
+        {
+            InitializeComponent();
+            EmployeeDataGrid.ItemsSource = adapter1.GetData();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            object Employee_code = (EmployeeDataGrid.SelectedItem as DataRowView).Row[0];
+            adapter1.DeleteEmployeeQuery(Convert.ToInt32(Employee_code));
+            EmployeeDataGrid.ItemsSource = adapter1.GetData();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            window.Show();
+            this.Close();
+        }
+
+ 
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Database.MagazineDataSetTableAdapters;
+namespace Database
+{
+    public partial class MainWindow : Window
+    {
+        RangTableAdapter adapter = new RangTableAdapter();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            RangDataGrid.ItemsSource = adapter.GetData();
+        }
+//Добавление данных в таблицу
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            try { 
+            object Rang_code = (RangDataGrid.SelectedItem as DataRowView).Row[0];
+            adapter.DeleteRangQuery(Convert.ToInt32(Rang_code));
+            RangDataGrid.ItemsSource = adapter.GetData();
+            }
+            catch
+            {
+                MessageBox.Show("Невозможно удалить данные");
+            }
+        }
+//Переход в другое окно
+        private void GoToNext_Click(object sender, RoutedEventArgs e)
+        {
+            EmployeeWindow window = new EmployeeWindow();
+            window.Show();
+            this.Close();
+        }
+    }
+}
